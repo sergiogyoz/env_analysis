54166665c8aac0df107f87535884cf1fe8fb376a
sergiogyoz <36090026+sergiogyoz@users.noreply.github.com>
2024-04-09 13:59:13 -0500

    testing wavelet package 1

---
+++ Spectral/MyStuff/testing.m
@@ -0,0 +1,22 @@
+basedir = cd;
+subdir1 = "\Spectral\wavelet-coherence-master";
+
+
+seriesname={'AO' 'BMI'};
+d1=load(basedir + subdir1 + '\faq\jao.txt');
+d2=load(basedir + subdir1 + '\faq\jbaltic.txt');
+
+d2(:,2)=boxpdf(d2(:,2));
+
+%plot(d2(:,1),d2(:,2));
+
+figure('color',[1 1 1])
+tlim=[min(d1(1,1),d2(1,1)) max(d1(end,1),d2(end,1))];
+subplot(2,1,1);
+wt(d1);
+title(seriesname{1});
+set(gca,'xlim',tlim);
+subplot(2,1,2)
+wt(d2)
+title(seriesname{2})
+set(gca,'xlim',tlim)
