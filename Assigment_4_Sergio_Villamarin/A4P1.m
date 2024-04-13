b7857d9c2402e8aa0b6174dbcc6dab86b14225ff
Carolina Hurtado-Pulido <carolinah0308@hotmail.com>
2024-04-13 00:07:43 -0500

    Almost done with the first part HW4

--- Assigment_4_Sergio_Villamarin/A4P1.m
+++ Assigment_4_Sergio_Villamarin/A4P1.m
@@ -1,29 +1,61 @@
-% normalize data to 0 mean and 1 std
-Qv_norm = normalize(Qv);
+%% normalize data to 0 mean and 1 std
+Qv_log_norm = normalize(log(Qv));
 stage_norm = normalize(stage);
+
 %% plot data and hist
 figure;
 plot(t, Qv);
 title("Discharge at Vicksburg")
 figure;
-plot(t, stage_norm);
-title("Stage at Grand isle")
+histogram(log(Qv));
+title("log discharge Vicksburg")
 figure;
-histogram(Qv_norm);
-title("normalized Vicksburg")
+histogram(Qv_log_norm);
+title("log normalized discharge Vicksburg")
+figure;
+plot(t, stage);
+title("Stage at Grand isle")
 figure;
 histogram(stage_norm);
 title("normalized Grand Isle")
-%% find the brakpoint in the stage data (apparent bimodal stage distribution)
-[location, X] = ischange(stage,"linear","MaxNumChanges",1);
-plot(t,stage)
-plot(t,X)
-breakpoint = find(location);
+
+%% find the breakpoint in the stage data (apparent bimodal stage distribution)
+[break_stage, X] = ischange(stage,"linear","MaxNumChanges",1);
+breakpoint = find(break_stage);
 breaktime = t(breakpoint);
-%% log normal test and transformation of discharge
-stage_log = log(stage);
-stage_log_norm = normalize(stage_log);
-histogram(stage_log_norm,30)
+
+%% split data into the two "almost normal" parts
+index = 1:length(t);
+before = (index < breakpoint);
+after = (index >= breakpoint);
+
+figure;
+histogram(stage_norm(before), 30);
+title("stage before the breakpoint");
+
+figure;
+histogram(stage_norm(after), 30);
+title("stage after the breakpoint");
+
+%% detrending and normal testing for stage (which it passes)
+lm_before = fitlm(t(before),stage_norm(before));
+stage_res_before = lm_before.Residuals.Raw;
+
+lm_after = fitlm(t(after),stage_norm(after));
+stage_res_after = lm_after.Residuals.Raw;
+
+stage_res = cat(1, stage_res_before, stage_res_after);
+histogram(stage_res, 15);
+[h, p] = adtest(stage_res);
+
+%% normalize residuals again as per request and plot
+stage_res_norm = normalize(stage_res);
+histogram(stage_res_norm, 19);
+
+%% FFT
+stage_fft = fft(stage_res_norm);
+
+
 
 
 
