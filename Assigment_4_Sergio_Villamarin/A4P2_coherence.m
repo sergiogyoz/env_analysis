99d9a8537dea0ec4f5cdbad752b3e87b14dda092
sergiogyoz <sergiogyoz@hotmail.com>
2024-04-22 23:20:22 -0500

    Update A4P2_coherence.m

--- Assigment_4_Sergio_Villamarin/A4P2_coherence.m
+++ Assigment_4_Sergio_Villamarin/A4P2_coherence.m
@@ -7,7 +7,7 @@ dj = 0.25;
 figure('color',[1 1 1])
 tiledlayout(2,2,'TileSpacing','compact');
 nexttile([1,2]);
-xwt([t Qv_final],[t stage_final], "Dj", dj)
+xwt([t Qv_final], [t stage_final], "Dj", dj)
 %%
 nexttile([1,2]);
-wtc(Qv_final,stage_final, "Dj", dj)  
+wtc([t Qv_final], [t stage_final], "Dj", dj)  
